6118ca34c6c46e7b956bb7ab48413b9a929904bd
Paul Reiter <reiter.paul@gmail.com>
2022-12-01 18:38:29 +0100

    Improve readability

--- 01/main.hs
+++ 01/main.hs
@@ -1,17 +1,15 @@
 import Data.List (sort)
 
+f :: String -> [[Int]] -> [[Int]]
+f [] acc = [] : acc
+f n acc = (read n : head acc) : tail acc
+
 parseInputFile :: [String] -> [[Int]]
-parseInputFile =
-  foldr
-    ( \x acc -> case x of
-        [] -> [] : acc
-        n -> (read n : head acc) : tail acc
-    )
-    [[]]
+parseInputFile = foldr f [[]]
 
 main :: IO ()
 main = do
-  content <- parseInputFile . lines <$> readFile "input.txt"
-  let calories = map sum content
+  input <- parseInputFile . lines <$> readFile "input.txt"
+  let calories = map sum input
   print $ maximum calories
   print $ sum . take 3 . reverse . sort $ calories
