871c726cff52bd23623ef3ed9be0ad9797f287ff
Paul Reiter <reiter.paul@gmail.com>
2022-12-02 16:19:31 +0100

    Add solution for day 2 part 2

--- 02/main.hs
+++ 02/main.hs
@@ -12,6 +12,15 @@ play Scissors Rock = Win2
 play Scissors Paper = Win1
 play Scissors Scissors = Draw
 
+whatToPlay :: RPS -> GameState -> RPS
+whatToPlay Rock Win1 = Scissors
+whatToPlay Rock Win2 = Paper
+whatToPlay Paper Win1 = Rock
+whatToPlay Paper Win2 = Scissors
+whatToPlay Scissors Win1 = Paper
+whatToPlay Scissors Win2 = Rock
+whatToPlay p1 Draw = p1
+
 parseLetter :: Char -> RPS
 parseLetter 'A' = Rock
 parseLetter 'B' = Paper
@@ -20,8 +29,16 @@ parseLetter 'X' = Rock
 parseLetter 'Y' = Paper
 parseLetter 'Z' = Scissors
 
+parseLetter2 :: Char -> GameState
+parseLetter2 'X' = Win1
+parseLetter2 'Y' = Draw
+parseLetter2 'Z' = Win2
+
 parseLetters :: String -> (RPS, RPS)
 parseLetters [p1, _, p2] = (parseLetter p1, parseLetter p2)
+
+parseLetters2 :: String -> (RPS, GameState)
+parseLetters2 [p1, _, p2] = (parseLetter p1, parseLetter2 p2)
 
 scoreRPS :: RPS -> Int
 scoreRPS Rock = 1
@@ -37,5 +54,8 @@ score :: (RPS, RPS) -> Int
 score (p1, p2) = scoreRPS p2 + scoreGameState (play p1 p2)
 
 main = do
-    strategyGuide <- map parseLetters . lines <$> readFile "input.txt"
+    content <- lines <$> readFile "input.txt"
+    let strategyGuide = map parseLetters content
     print $ sum $ map score strategyGuide
+    let strategyGuide = map parseLetters2 content
+    print $ sum $ map (score . (\(p, g) -> (p, whatToPlay p g))) strategyGuide
