514150c615a182e81996cb1529700dadab69bc16
Paul Reiter <reiter.paul@gmail.com>
2022-12-03 17:50:08 +0100

    Add solution for day 3 part 2

--- 03/main.hs
+++ 03/main.hs
@@ -1,20 +1,34 @@
 import Data.Char (ord, isLower, isUpper)
-data Rucksack = Rucksack {c1 :: String, c2 :: String} deriving (Show)
 
-parseInput :: String -> [Rucksack]
-parseInput s = map parseLine $ lines s
-    where parseLine line = Rucksack (take half line) (drop half line)
-            where half = div (length line) 2
+type Rucksack = String
+type Group = (Rucksack, Rucksack, Rucksack)
 
 getPriority :: Char -> Int
 getPriority l
     | isLower l = ord l - 96
     | isUpper l = ord l - 38
 
+findCommonLetters :: String -> String -> String
+findCommonLetters s1 = filter (`elem` s1)
+
+findCommonLetter :: String -> String -> Char
+findCommonLetter s1 s2 = head $ findCommonLetters s1 s2
+
 findCommonItem :: Rucksack -> Char
-findCommonItem Rucksack {c1, c2} = head $ filter (`elem` c1) c2
+findCommonItem rucksack = findCommonLetter c1 c2
+    where half = div (length rucksack) 2
+          c1 = take half rucksack
+          c2 = drop half rucksack
+
+findCommonGroupItem :: Group -> Char
+findCommonGroupItem (r1, r2, r3) = findCommonLetter r1 $ findCommonLetters r2 r3
+
+groupRucksacks :: [Rucksack] -> [Group]
+groupRucksacks [] = []
+groupRucksacks (r1:r2:r3:rs) = (r1, r2, r3) : groupRucksacks rs
 
 main :: IO ()
 main = do
-    rucksacks <- parseInput <$> readFile "input.txt"
+    rucksacks <- lines <$> readFile "input.txt"
     print $ sum $ map (getPriority . findCommonItem) rucksacks
+    print $ sum $ map (getPriority . findCommonGroupItem) $ groupRucksacks rucksacks
