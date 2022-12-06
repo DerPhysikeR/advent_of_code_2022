4636659ad1480256bddc94e5710406e3d27ba228
Paul Reiter <reiter.paul@gmail.com>
2022-12-06 17:36:23 +0100

    Unify implementations of part 1 and 2

--- 06/main.hs
+++ 06/main.hs
@@ -7,21 +7,18 @@ allDifferent :: Datastream -> Bool
 allDifferent [] = True
 allDifferent (x:xs) = x `notElem` xs && allDifferent xs
 
-findStartOfPacket :: Index -> Datastream -> Maybe Int
-findStartOfPacket i xs@(a:b:c:d:_)
-    | allDifferent [a, b, c, d] = Just (i + 3)
-    | otherwise = findStartOfPacket (i + 1) (tail xs)
-findStartOfPacket _ _ = Nothing
-
 takeMaybe :: Int -> [a] -> Maybe [a]
 takeMaybe n xs = if len < n then Nothing else Just taken
     where len = length taken
           taken = take n xs
 
-findStartOfMessage :: Index -> Datastream -> Maybe Int
-findStartOfMessage i xs = case takeMaybe 14 xs of
+findFirstInstanceOfNDifferent :: Int -> Int -> Datastream -> Maybe Int
+findFirstInstanceOfNDifferent n i xs = case takeMaybe n xs of
     Nothing -> Nothing
-    Just ys -> if allDifferent ys then Just (i + 13) else findStartOfMessage (i + 1) (tail xs)
+    Just ys -> if allDifferent ys then Just (i + n - 1) else findFirstInstanceOfNDifferent n (i + 1) (tail xs)
+
+findStartOfPacket = findFirstInstanceOfNDifferent 4
+findStartOfMessage = findFirstInstanceOfNDifferent 14
 
 main = do
     -- tests
@@ -37,11 +34,13 @@ main = do
 
     let error_message = "error in findStartOfMessage"
     unless (isNothing $ findStartOfMessage 1 "abc") (error error_message)
+    unless (findStartOfMessage 1 "abcdefghijklmn" == Just 14) (error error_message)
     unless (findStartOfMessage 1 "mjqjpqmgbljsphdztnvjfqwrcgsmlb" == Just 19) (error error_message)
     unless (findStartOfMessage 1 "bvwbjplbgvbhsrlpgdmjqwftvncz" == Just 23) (error error_message)
     unless (findStartOfMessage 1 "nppdvjthqldpwncqszvftbrmjlhg" == Just 23) (error error_message)
     unless (findStartOfMessage 1 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" == Just 29) (error error_message)
     unless (findStartOfMessage 1 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" == Just 26) (error error_message)
+
     -- main
     input <- head . lines <$> readFile "input.txt"
     print $ findStartOfPacket 1 input
