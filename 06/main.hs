61e79f9864219297fd92935f7ba31a24b6879e8e
Paul Reiter <reiter.paul@gmail.com>
2022-12-06 17:28:20 +0100

    Add solution for day 6 part 2

--- 06/main.hs
+++ 06/main.hs
@@ -13,6 +13,16 @@ findStartOfPacket i xs@(a:b:c:d:_)
     | otherwise = findStartOfPacket (i + 1) (tail xs)
 findStartOfPacket _ _ = Nothing
 
+takeMaybe :: Int -> [a] -> Maybe [a]
+takeMaybe n xs = if len < n then Nothing else Just taken
+    where len = length taken
+          taken = take n xs
+
+findStartOfMessage :: Index -> Datastream -> Maybe Int
+findStartOfMessage i xs = case takeMaybe 14 xs of
+    Nothing -> Nothing
+    Just ys -> if allDifferent ys then Just (i + 13) else findStartOfMessage (i + 1) (tail xs)
+
 main = do
     -- tests
     let error_message = "error in findStartOfPacket"
@@ -24,6 +34,15 @@ main = do
     unless (findStartOfPacket 1 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" == Just 11) (error error_message)
     unless (isNothing $ findStartOfPacket 1 "aaaaaaaaaaaaaaaa") (error error_message)
     unless (isNothing $ findStartOfPacket 1 "abc") (error error_message)
+
+    let error_message = "error in findStartOfMessage"
+    unless (isNothing $ findStartOfMessage 1 "abc") (error error_message)
+    unless (findStartOfMessage 1 "mjqjpqmgbljsphdztnvjfqwrcgsmlb" == Just 19) (error error_message)
+    unless (findStartOfMessage 1 "bvwbjplbgvbhsrlpgdmjqwftvncz" == Just 23) (error error_message)
+    unless (findStartOfMessage 1 "nppdvjthqldpwncqszvftbrmjlhg" == Just 23) (error error_message)
+    unless (findStartOfMessage 1 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" == Just 29) (error error_message)
+    unless (findStartOfMessage 1 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" == Just 26) (error error_message)
     -- main
     input <- head . lines <$> readFile "input.txt"
     print $ findStartOfPacket 1 input
+    print $ findStartOfMessage 1 input
