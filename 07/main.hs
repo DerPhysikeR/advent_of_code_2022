9fb17dd3d76a6adf725a8aa06e87e232dfd223db
Paul Reiter <reiter.paul@gmail.com>
2022-12-07 18:09:41 +0100

    Parse files to flat file map

--- 07/main.hs
+++ 07/main.hs
@@ -1,8 +1,13 @@
 import Data.Map qualified as M
 import Data.List (foldl')
+import Data.Char (isDigit)
 
+data FileObject = File {name :: String, size :: Int} | Directory {name :: String} deriving (Show)
 type Path = [String]
-type State = (Path, M.Map Path [String])
+type State = (Path, M.Map Path [FileObject])
+
+parseFile :: String -> FileObject
+parseFile s = File (tail $ dropWhile isDigit s) (read $ takeWhile isDigit s)
 
 parseLine :: State -> String -> State
 parseLine (p, m) ('$':' ':'c':'d':' ':dir)
@@ -10,9 +15,10 @@ parseLine (p, m) ('$':' ':'c':'d':' ':dir)
     | dir == ".." = (init p, m)
     | otherwise = (p ++ [dir], m)
 parseLine s ('$':' ':'l':'s':_) = s
-parseLine (p, m) line = (p, M.insertWith (++) p [line] m)
+parseLine (p, m) ('d':'i':'r':' ':dir) = (p, M.insertWith (++) p [Directory dir] m)
+parseLine (p, m) line = (p, M.insertWith (++) p [parseFile line] m)
 
 main :: IO ()
 main = do
-    input <- foldl' parseLine ([""], M.empty) . lines <$> readFile "test_input.txt"
-    print input
+    (_, filemap) <- foldl' parseLine ([""], M.empty) . lines <$> readFile "test_input.txt"
+    print filemap
