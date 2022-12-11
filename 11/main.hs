1bbc6c6c7f9e618dea47effb209955e64b13df82
Paul Reiter <reiter.paul@gmail.com>
2022-12-11 17:27:37 +0100

    Add solution for day 11 part 1

--- 11/main.hs
+++ 11/main.hs
@@ -3,17 +3,19 @@ import Data.Text qualified as T
 import Data.Text.IO qualified as TIO
 import Data.Sequence qualified as S
 import Debug.Trace (traceShow)
-import Data.Foldable (Foldable(foldl'))
+import Data.Foldable (Foldable(foldl'), toList)
+import Data.Foldable qualified as F
+import Data.List (sort)
 tr x = traceShow x x
 
-data Monkey = Monkey {items :: [Int], operation :: Int -> Int, throwTo :: Int -> Int}
+data Monkey = Monkey {items :: [Int], operation :: Int -> Int, throwTo :: Int -> Int, activity :: Int}
 type Monkeys = S.Seq Monkey
 
 instance Show Monkey where
-    show (Monkey items operation throwTo) = "Monkey " ++ show items
+    show (Monkey items operation throwTo activity) = "Monkey " ++ show items ++ " " ++ show activity
 
 parseMonkey :: T.Text -> Monkey
-parseMonkey t = Monkey items operation (\x -> if (x `mod` divisor) == 0 then trueMonkey else falseMonkey)
+parseMonkey t = Monkey items operation (\x -> if (x `mod` divisor) == 0 then trueMonkey else falseMonkey) 0
     where (_:itemLine:opLine:testLine:ifTrueLine:ifFalseLine:_) = T.lines t
           afterColon = last . T.splitOn ": "
           afterOld = last . T.splitOn " old "
@@ -30,17 +32,17 @@ parseMonkey t = Monkey items operation (\x -> if (x `mod` divisor) == 0 then tru
 
 monkeyThrow :: Int -> Monkeys -> Monkeys
 monkeyThrow idx monkeys = case S.index monkeys idx of
-    Monkey [] _ _ -> monkeys
-    Monkey (item:items) operation throwTo -> monkeysItemCaught
-        where monkeysItemThrown = S.update idx (Monkey items operation throwTo) monkeys
+    Monkey [] _ _ _ -> monkeys
+    Monkey (item:items) operation throwTo activity -> monkeysItemCaught
+        where monkeysItemThrown = S.update idx (Monkey items operation throwTo (activity + 1)) monkeys
               worryLevel = div (operation item) 3
               throwToIdx = throwTo worryLevel
-              catchingMonkey@(Monkey it o tt) = S.index monkeysItemThrown throwToIdx
-              monkeysItemCaught = S.update throwToIdx (Monkey (it ++ [worryLevel]) o tt) monkeysItemThrown
+              catchingMonkey@(Monkey it o tt a) = S.index monkeysItemThrown throwToIdx
+              monkeysItemCaught = S.update throwToIdx (Monkey (it ++ [worryLevel]) o tt a) monkeysItemThrown
 
 monkeyTurn :: Int -> Monkeys -> Monkeys
 monkeyTurn idx monkeys = case S.index monkeys idx of
-    Monkey [] _ _ -> monkeys
+    Monkey [] _ _ _ -> monkeys
     _ -> monkeyTurn idx (monkeyThrow idx monkeys)
 
 monkeyRound :: Monkeys -> Monkeys
@@ -48,6 +50,7 @@ monkeyRound monkeys = foldl' (flip monkeyTurn) monkeys [0..length monkeys - 1]
 
 main :: IO ()
 main = do
-    monkeys <- S.fromList . map parseMonkey . T.splitOn "\n\n" <$> TIO.readFile "test_input.txt"
+    monkeys <- S.fromList . map parseMonkey . T.splitOn "\n\n" <$> TIO.readFile "input.txt"
     print monkeys
-    print $ last $ take 21 $ iterate monkeyRound monkeys
+    let finishedMonkeys = last $ take 21 $ iterate monkeyRound monkeys
+    print $ product $ take 2 $ reverse $ sort $ map (\(Monkey _ _ _ activity) -> activity) (F.toList finishedMonkeys)
